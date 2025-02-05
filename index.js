53456563a2be6fa38d02c2c523ca2bc6204fe0e4
Dmitry <mityourik@gmail.com>
2025-02-05 23:49:50 +0200

    fix rollup settings and layout with styles

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function hello() {
-  console.log("Hello world!");
-}
- 
-hello();
+import JSimg from './assets/js-image.png';
+import './index.css';
+
+const title = document.createElement("h1");
+title.innerHTML = "I love JavaScript";
+
+const img = document.createElement("img");
+img.src = JSimg;
+
+document.body.append(title);
+document.body.append(img);
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,36 @@
+import styles from "rollup-plugin-styles";
+import image from '@rollup/plugin-image';
+import babel from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import copy from 'rollup-plugin-copy';
+
 export default {
   input: './index.js',
   output: {
     file: './build/bundle.js',
-    format: 'cjs'
-  }
+    format: 'cjs',
+    assetFileNames: "[name]-[hash][extname]",
+  },
+  plugins: [
+    styles(),
+    image(),
+    babel({ 
+        babelHelpers: 'bundled',
+        presets: ['@babel/preset-env'],
+        extensions: [".js"],
+        exclude: "node_modules/**",
+    }),
+    serve({
+      open: true,
+      contentBase: 'build',
+      port: 3000
+    }),
+    livereload('build'),
+    copy({
+      targets: [
+        { src: 'index.html', dest: 'build' }
+      ]
+    })
+  ],
 };
